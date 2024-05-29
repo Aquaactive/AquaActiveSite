a7be117c1ff3a4ba9bf20ca6fb1a8ad8cbeb2fa7
Aquaactive <yos.tarik07@gmail.com>
2024-05-29 16:18:03 +0300

    Update WhatsappButton.tsx

--- app/components/WhatsappButton.tsx
+++ app/components/WhatsappButton.tsx
@@ -48,7 +48,7 @@ const WhatsappButton = () => {
             }
             transition={{ duration: 0.5, ease: "easeInOut" }}
             className='h-16 w-16 rounded-full cardWP fixed flex items-center justify-center bottom-6 right-6 z-20'>
-            <Link href="https://wa.me/905439466" target="_blank" rel="noopener noreferrer">
+            <Link href="https://wa.me/905435039466" target="_blank" rel="noopener noreferrer">
                 <Image src='/wp.webp' alt='whatsapp' width={36} height={36} />
             </Link>
         </motion.div>
