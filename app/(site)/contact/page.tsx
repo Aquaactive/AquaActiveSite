82a38bdf99adf6968615fc18c035856ccd9d3fa2
Aquaactive <yos.tarik07@gmail.com>
2024-05-29 16:18:48 +0300

    Update page.tsx

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -17,13 +17,13 @@ const Contact = () => {
               <span>Çarşı Mah. Seral Sk. No 17 ANTALYA/ALANYA</span>
             </a>
             <a
-              href="tel:+905439466"
+              href="tel:+905435039466"
               target='_blank'
               className="flex w-fit items-center text-2xl">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
-              <span>+90 543 9466</span>
+              <span>+90 543 503 9466</span>
             </a>
             <a
               href="mailto:info@aquaactive.com"
